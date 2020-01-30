d1662b056775e633beb5f5a2b313f949e71abf89
ryanbittybyte <ryandumelie@gmail.com>
2020-01-29 21:37:16 -0600

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      alert("Great Job, you pushed the button!!");
+    });
+  }
